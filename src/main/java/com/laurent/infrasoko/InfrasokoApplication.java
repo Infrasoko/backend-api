7b9d24074c3f76f1ed74cddf932a7bf0488fe5af
Laurent-I <irakramlaw@gmail.com>
2025-01-13 15:10:02 +0200

    Initial commit: Project setup

---
+++ src/main/java/com/laurent/infrasoko/InfrasokoApplication.java
@@ -0,0 +1,13 @@
+package com.laurent.infrasoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrasokoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrasokoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurent/infrasoko/InfrasokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurent.infrasoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrasokoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
